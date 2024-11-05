8745da797d0b3fd3d50b26edffcc582b8a5877f5
joshcord99 <joshcord99@gmail.com>
2024-11-04 20:45:36 -0800

    added to auth.ts

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -6,15 +6,22 @@ class AuthService {
     // TODO: return the decoded token
     return jwtDecode<UserData>(this.getToken());
   }
-
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
   }
 
   isTokenExpired(token: string) {
     try {
       // TODO: return a value that indicates if the token is expired
+      const decoded = jwtDecode<JwtPayload>(token);
+      if (decoded?.exp && decoded?.exp < Date.now() / 1000) {
+        return true;
       }
+    } catch (err) {
+      return false;
+    }
   }
 
   getToken(): string {
